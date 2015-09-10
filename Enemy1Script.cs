bed9053b8aa19f461557685acca3c3ea13cec26e
KarateF22 <KarateF22@gmail.com>
2015-09-10 11:03:46 -0400

    Player and Simple Enemy Chase Script
    Simple scripts for group review

---
+++ Enemy1Script.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy1Script : MonoBehaviour {
+	public Transform target;
+	public Vector2 speed = new Vector2(5, 5);
+	private Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 direction = (target.position - transform.position).normalized;
+		movement = new Vector2 (speed.x * direction.x, speed.y * direction.y);
+	}
+
+	void FixedUpdate() {
+		GetComponent<Rigidbody2D>().velocity = movement;
+	}
+}
