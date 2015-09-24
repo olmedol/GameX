9596e666c8f2990bbb7b424b5cd870d97d3f2fe6
Kevin Ashline <kwashlin@uncg.edu>
2015-09-24 13:21:23 -0400

    Boundary & Camera Zoom scripts added
    Boundary adds x/y limitations on character (can be added to any game
    object, I believe) camera zoom function allows player to zoom in/out
    with mouse scroll wheel, range of zoom can be easily changed in the
    script.

---
+++ Assets/Standard Assets/2D/Scripts/Boundary.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boundary : MonoBehaviour {
+	public float minY = 0.0f, minX = 0.0f, maxY = 0.0f, maxX = 0.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 currentPosition = transform.position;
+		currentPosition.y = Mathf.Clamp (currentPosition.y, minY, maxY);
+		currentPosition.x = Mathf.Clamp (currentPosition.x, minX, maxX);
+		transform.position = currentPosition;
+	}
+}
