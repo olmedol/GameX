e5e1afe3e90043401d198d3b3d3865e1dbc67934
esnursey <esnursey@uncg.edu>
2015-10-07 17:46:28 -0400

    Pause Menu Added
    Added a pause menu for the game, only available during actual gameplay.
    Exit to Main Menu button not currently functional.

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGame : MonoBehaviour {
+
+	public GameObject PauseGUI;
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown ("escape")) {
+
+			Time.timeScale = 0;
+			PauseGUI.SetActive(true);
+
+
+
+
+
+		}
+	
+	}
+}
---
+++ Assets/Scripts/ResumeGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResumeGame : MonoBehaviour {
+
+	public GameObject PauseGUI;
+
+	public void resumeGame(){
+
+		Time.timeScale = 1;
+		PauseGUI.SetActive (false);
+
+
+	}
+
+
+
+
+}
