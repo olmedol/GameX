63c72311e9c4943321a8313c81ccb3a2b3b6f0a6
olmedol <l_olmedo@uncg.edu>
2015-10-26 23:01:27 -0400

    Multiplayer
    Enemy spawns is disabled, this may be reverted to avoid messing with
    others code.

---
+++ Assets/Scripts/Multiplayer/NetworkManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkManager : MonoBehaviour {
+	const string VERSION = "v0.0.1";
+	public string roomName = "VVR";
+	public string playerPrefabName = "Player";
+	public Transform spawnPoint;
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings (VERSION);
+	}
+
+	void OnJoinedLobby(){
+		RoomOptions roomOptions = new RoomOptions () { isVisible = false, maxPlayers = 4};
+		PhotonNetwork.JoinOrCreateRoom (roomName, roomOptions, TypedLobby.Default);
+
+	}
+
+
+	void OnJoinedRoom() { 
+		print(PhotonNetwork.isMasterClient);
+
+		PhotonNetwork.Instantiate (playerPrefabName,
+		                          spawnPoint.position, 
+		                          spawnPoint.rotation,
+		                          0);
+
+	}
+
+}
