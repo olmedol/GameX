e4e13000f3830d7ab26e537a069d47378a2160db
Kevin Ashline <kwashlin@uncg.edu>
2015-10-29 17:11:09 -0400

    New art, Asteroids!
    Art for the enemies missing art assets as well as 4 new types of
    asteroid, with a *broken* spawn script.

---
+++ Assets/Asteroid.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : Photon.MonoBehaviour {
+	private int speed;
+	private Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range(3,20);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PhotonNetwork.isMasterClient) {
+			Vector2 direction = new Vector2(Random.value, Random.value);
+			movement = direction * speed;
+			transform.Rotate(direction);
+			
+		}
+
+	}
+	void FixedUpdate() {
+		if (PhotonNetwork.isMasterClient)
+			GetComponent<Rigidbody2D> ().velocity = movement;
+	}
+}
