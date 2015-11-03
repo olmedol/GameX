71771aed41673ff879c0ef56ab7ff94c7817aa2c
olmedol <l_olmedo@uncg.edu>
2015-11-02 19:00:32 -0500

    Separating Single and Multi

---
+++ Assets/Scripts/Multiplayer/NetworkManagerSingle.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkManagerSingle : MonoBehaviour {
+	const string VERSION = "v0.0.1";
+	public string roomName = "VVR";
+	public string playerPrefabName = "Player";
+	public Transform spawnPoint;
+	// Use this for initialization
+	void Start () {
+		//comment out connectusingsettings line if you want offline. 
+		PhotonNetwork.offlineMode = true;
+		PhotonNetwork.CreateRoom (roomName);
+		//PhotonNetwork.ConnectUsingSettings (VERSION);
+	}
+	
+	void OnJoinedLobby(){
+		
+		//Don't use these 2 lines if you wan't offline mode to work
+		//RoomOptions roomOptions = new RoomOptions () { isVisible = false, maxPlayers = 4};
+	//	PhotonNetwork.JoinOrCreateRoom (roomName, roomOptions, TypedLobby.Default);
+		
+	}
+	
+	
+	void OnJoinedRoom() { 
+		print(PhotonNetwork.isMasterClient);
+		
+		PhotonNetwork.Instantiate (playerPrefabName,
+		                           spawnPoint.position, 
+		                           spawnPoint.rotation,
+		                           0);
+		
+	}
+	
+}
