b30e2bc64d0df86d56c6505fb5484bf0395e69d5
KarateF22 <KarateF22@gmail.com>
2015-11-20 12:05:08 -0500

    Fixed Multiplayer button not working after having played

--- Assets/Scripts/LoadOnClick.cs
+++ Assets/Scripts/LoadOnClick.cs
@@ -6,9 +6,16 @@ public class LoadOnClick : MonoBehaviour {
 	public GameObject loadingImage;
 	public static bool onlineStatus;
 	
+	public void LoadGame(bool online){
+		onlineStatus = online;
+		LoadScene (1);
+	}
+
 	public void LoadScene(int level)
 	{
-		onlineStatus = (level!=0);
+		if (PhotonNetwork.connected)
+			PhotonNetwork.Disconnect ();
+		//onlineStatus = (level!=0);
 		//loadingImage.SetActive(true);
 		Time.timeScale = 1;
 		Application.LoadLevel(level);
